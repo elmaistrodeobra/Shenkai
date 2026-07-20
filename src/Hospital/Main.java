0ac78e8f88e71730159364248b16433d3cc17e3c
blest <blest@shoujoyo>
2026-07-20 17:40:37 -0600

    Angel Ulises va a caer

--- src/Hospital/Main.java
+++ src/Hospital/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication12;
+package Hospital;
 
 /**
  *
  * @author uli08
  */
-public class JavaApplication12 {
+public class Main {
 
     /**
      * @param args the command line arguments
