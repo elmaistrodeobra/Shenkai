148adc604f521c67654c5220940aa2bd3ce67090
uli08 <uli08@uliECM>
2026-07-20 16:49:21 -0600

    comentario

--- src/javaapplication12/JavaApplication12.java
+++ src/javaapplication12/JavaApplication12.java
@@ -14,7 +14,7 @@ public class JavaApplication12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       //hola pellejos soy el anticristo 2009
     }
     
 }
