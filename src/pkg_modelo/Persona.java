05ed567c30c926fdb97701fe82ee3a82fbceb656
blest <blest@shoujoyo>
2026-07-20 18:08:02 -0600

    los amo

---
+++ src/pkg_modelo/Persona.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg_modelo;
+
+/**
+ *
+ * @author uli08
+ */
+public abstract class Persona {
+    protected int id;
+    protected String nombre;
+    protected String apellido;
+    protected int edad;
+    protected int telefono;
+    
+    public Persona (int i, String n,String a, int e, int t){
+        this.id=i;
+        this.nombre=n;
+        this.apellido=a;
+        this.edad=e;
+        this.telefono=t;
+    }
+    public abstract String mostrarPerfil ();
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+}
